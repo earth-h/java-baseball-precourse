22bf6414f00f82e1ec652b08d3d71b710fc62c66
earth-h <earth.h.183@gmail.com>
2022-10-01 22:17:29 +0900

    refactor(domain): modify differentRandomNumberGenerator variable
    Set differentRandomNumberGenerator.generateNumber

--- src/main/java/baseball/model/DifferentRandomNumberGenerator.java
+++ src/main/java/baseball/model/DifferentRandomNumberGenerator.java
@@ -10,13 +10,13 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
  */
 public class DifferentRandomNumberGenerator implements NumberGenerator {
 
+    private final Map<Integer, Integer> generateNumber = new HashMap<>(); // 각 자리수가 서로 달라야 하므로 List가 아닌 Map 사용 (key: 숫자, value: 해당 숫자가 존재하는 자리)
+
     public DifferentRandomNumberGenerator() {
     }
 
     @Override
     public Map<Integer, Integer> generateNumber(NumberControl numberControl) {
-        Map<Integer, Integer> generateNumber = new HashMap<>(); // 각 자리수가 서로 달라야 하므로 List가 아닌 Map 사용 (key: 숫자, value: 해당 숫자가 존재하는 자리)
-
         while(generateNumber.size() < numberControl.getNumberTotalLength()) {
             int number = pickNumberInRange(numberControl.getStartInclusive(), numberControl.getEndInclusive());
             generateNumber.putIfAbsent(number, generateNumber.size() + 1);
